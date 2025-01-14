68a9ec5f5aaec814b3e75b03216463dab80b73ef
torlandes <torlandesx@gmial.com>
2025-01-15 02:44:14 +0300

    Added utils scripts

---
+++ Assets/Scripts/Utils/Log/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace TDS.Utils.Log
+{
+    public static class Logger
+    {
+        #region Public methods
+
+        public static void Error(this object obj, object message = null, [CallerMemberName] string memberName = default)
+        {
+            Debug.LogError(FormatMessage(obj.GetType(), memberName, message));
+        }
+
+        [Conditional("UNITY_EDITOR")] [Conditional("DEBUG")]
+        public static void Log(this object obj, object message = null, [CallerMemberName] string memberName = default)
+        {
+            Debug.Log(FormatMessage(obj.GetType(), memberName, message));
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string FormatMessage(Type type, string memberName, object message)
+        {
+            string prefix = !Application.isEditor
+                ? $"[{Time.frameCount}]"
+                : $"[{DateTime.Now.ToString("HH:mm:ss")}:{Time.frameCount}]";
+            return $"{prefix} [{type.Name} : {memberName}] {message}";
+        }
+
+        #endregion
+    }
+}
