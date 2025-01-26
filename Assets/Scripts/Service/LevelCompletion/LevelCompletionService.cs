98b861343d1c5c70a3aee4e8e2e3c327d6d8a6f5
torlandes <torlandesx@gmial.com>
2025-01-27 01:27:08 +0300

    Added level Completion service

---
+++ Assets/Scripts/Service/LevelCompletion/LevelCompletionService.cs
@@ -0,0 +1,64 @@
+﻿using TDS.Infrastructure.Locator;
+using TDS.Service.LevelLoading;
+using TDS.Service.Mission;
+using TDS.Utils.Log;
+
+namespace TDS.Service.LevelCompletion
+{
+    public class LevelCompletionService : IService
+    {
+        #region Variables
+
+        private readonly MissionService _missionService;
+        private readonly LevelLoadingService _levelLoadingService;
+
+        #endregion
+
+        #region Setup/Teardown
+
+        public LevelCompletionService(MissionService missionService, LevelLoadingService levelLoadingService)
+        {
+            _missionService = missionService;
+            _levelLoadingService = levelLoadingService;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Dispose()
+        {
+            _missionService.OnCompleted -= MissionCompletedCallback;
+        }
+
+        public void Initialize()
+        {
+            _missionService.OnCompleted += MissionCompletedCallback;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void MissionCompletedCallback()
+        {
+            this.Error();
+            
+            //TODO: Show win screen
+            //Enter next level
+
+            if (_levelLoadingService.HasNextLevel())
+            {
+                _levelLoadingService.EnterNextLevel();
+            }
+            
+            else
+            {
+                //TODO:
+                this.Error($"GAME OVER!");
+            }
+        }
+
+        #endregion
+    }
+}
