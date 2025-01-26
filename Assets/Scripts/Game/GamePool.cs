1683d3d628056ccd613cea06b9922ce143b6add4
torlandes <torlandesx@gmial.com>
2025-01-27 01:28:25 +0300

    Added game pool

---
+++ Assets/Scripts/Game/GamePool.cs
@@ -0,0 +1,28 @@
+﻿using Lean.Pool;
+using UnityEngine;
+
+namespace TDS.Game
+{
+    public class GamePool
+    {
+        #region Public methods
+
+        public static void Despawn(GameObject go)
+        {
+            LeanPool.Despawn(go);
+        }
+
+        public static void Spawn(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent = null)
+        {
+            LeanPool.Spawn(prefab, position, rotation, parent);
+        }
+
+        public static void Spawn<T>(T prefab, Vector3 position, Quaternion rotation, Transform parent = null)
+            where T : Component
+        {
+            LeanPool.Spawn(prefab, position, rotation, parent);
+        }
+
+        #endregion
+    }
+}
