32c8aa1d58daa4f843f96bbd9386b8ab3692a609
torlandes <torlandesx@gmial.com>
2025-01-27 01:26:53 +0300

    Added level loading service

---
+++ Assets/Scripts/Service/LevelLoading/LevelLoadingConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDS.Service.LevelLoading
+{
+    [CreateAssetMenu(fileName = nameof(LevelLoadingConfig), menuName = "Game/LevelLoading/Level Loading Config")]
+    public class LevelLoadingConfig : ScriptableObject
+    {
+        #region Variables
+
+        [SerializeField] private string[] _levelSceneNames;
+
+        #endregion
+
+        #region Properties
+
+        public IReadOnlyList<string> LevelSceneNames => _levelSceneNames;
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Service/LevelLoading/LevelLoadingService.cs
@@ -0,0 +1,71 @@
+﻿using TDS.Infrastructure.Locator;
+using TDS.Infrastructure.State;
+using TDS.Utils.Log;
+using UnityEngine;
+
+namespace TDS.Service.LevelLoading
+{
+    public class LevelLoadingService : IService
+    {
+        #region Variables
+
+        private const string ConfigPath = "Configs/Levels/LevelLoadingConfig";
+
+        private readonly StateMachine _stateMachine;
+
+        private LevelLoadingConfig _config;
+        private int _levelIndex;
+
+        #endregion
+
+        #region Setup/Teardown
+
+        public LevelLoadingService(StateMachine stateMachine)
+        {
+            _stateMachine = stateMachine;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void EnterFirstLevel()
+        {
+            _levelIndex = 0;
+            EnterLevel();
+        }
+
+        public void EnterNextLevel()
+        {
+            if (!HasNextLevel())
+            {
+                this.Error("AHTUNG!");
+                return;
+            }
+
+            _levelIndex++;
+            EnterLevel();
+        }
+
+        public bool HasNextLevel()
+        {
+            return _levelIndex < _config.LevelSceneNames.Count - 1;
+        }
+
+        public void Initialize()
+        {
+            _config = Resources.Load<LevelLoadingConfig>(ConfigPath);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void EnterLevel()
+        {
+            _stateMachine.Enter<LoadGameState, string>(_config.LevelSceneNames[_levelIndex]);
+        }
+
+        #endregion
+    }
+}
