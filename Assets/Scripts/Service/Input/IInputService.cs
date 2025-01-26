b8643bec14dd28ba12ffefff856995b6f14525a0
torlandes <torlandesx@gmial.com>
2025-01-27 01:27:19 +0300

    Added Input service

---
+++ Assets/Scripts/Service/Input/IInputService.cs
@@ -0,0 +1,21 @@
+﻿using TDS.Infrastructure.Locator;
+using UnityEngine;
+
+namespace TDS.Service.Input
+{
+    public interface IInputService : IService
+    {
+        #region Properties
+
+        Vector3 LookDirection { get; }
+        Vector2 MoveDirection { get; }
+
+        #endregion
+
+        #region Public methods
+
+        bool IsAttackClicked();
+
+        #endregion
+    }
+}
